e05cb0f8cff2a8c321d7d7dd3d83cbbf0cd63fc7
mrxiaozhuox <mrxzx@wwsg18.top>
2021-09-15 12:48:43 +0800

    feat: commit code

--- init.lua
+++ init.lua
@@ -4,18 +4,8 @@ local manager = require("manager")
 
 -- include plugin here
 
-manager.load("example", { ["token"] = "DOREA@TEST" })
+manager.load("example", { })
 
 -- end to include
 
 manager.require(ROOT_PATH)
-
-function CallEvent(event)
-    if event == "onload" then
-        return manager.call_onload()
-    elseif event == "unload" then
-        return manager.call_unload()
-    elseif event == "interval" then
-        return manager.call_interval()
-    end
-end
--- library/interface.lua
+++ library/interface.lua
@@ -2,6 +2,8 @@ local interface = {}
 
 interface["func"] = {}
 interface["setting"] = {}
+
 interface["db"] = DB_MANAGER
+interface["log"] = LOGGER_IN
 
 return interface
